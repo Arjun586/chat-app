8112e3117773be844d90a67e66506173fb3ac3a4
Arjun <121486212+Arjun586@users.noreply.github.com>
2025-12-20 14:51:17 +0530

    Add UI

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+
+const App = () => {
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
